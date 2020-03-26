a08f408e4334cc7d3a2486c35817c648d6cd1b6a
Emerson Diego <diegowebby@gmail.com>
2020-03-26 09:25:54 -0300

    ajustes para requisições funcionarem

--- src/main/java/br/com/alura/forum/config/security/AutenticacaoViaTokenFilter.java
+++ src/main/java/br/com/alura/forum/config/security/AutenticacaoViaTokenFilter.java
@@ -15,42 +15,57 @@ import br.com.alura.forum.modelo.Usuario;
 import br.com.alura.forum.repository.UsuarioRepository;
 
 public class AutenticacaoViaTokenFilter extends OncePerRequestFilter {
-	
-	private TokenService tokenService;
-	private UsuarioRepository repository;
 
-	public AutenticacaoViaTokenFilter(TokenService tokenService, UsuarioRepository repository) {
-		this.tokenService = tokenService;
-		this.repository = repository;
-	}
+    private TokenService tokenService;
+    private UsuarioRepository repository;
 
-	@Override
-	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-			throws ServletException, IOException {
-		
-		String token = recuperarToken(request);
-		boolean valido = tokenService.isTokenValido(token);
-		if (valido) {
-			autenticarCliente(token);
-		}
-		
-		filterChain.doFilter(request, response);
-	}
+    public AutenticacaoViaTokenFilter(TokenService tokenService, UsuarioRepository repository) {
+        this.tokenService = tokenService;
+        this.repository = repository;
+    }
 
-	private void autenticarCliente(String token) {
-		Long idUsuario = tokenService.getIdUsuario(token);
-		Usuario usuario = repository.findById(idUsuario).get();
-		UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
-		SecurityContextHolder.getContext().setAuthentication(authentication);
-	}
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
 
-	private String recuperarToken(HttpServletRequest request) {
-		String token = request.getHeader("Authorization");
-		if (token == null || token.isEmpty() || !token.startsWith("Bearer ")) {
-			return null;
-		}
-		
-		return token.substring(7, token.length());
-	}
+        System.out.println("WebConfig; " + request.getRequestURI());
+        // request.setHeader("Bearer
+        // eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJBUEkgZG8gRsOzcnVtIGRhIEFsdXJhIiwic3ViIjoiMSIsImlhdCI6MTU4NTE4NTI5NywiZXhwIjoxNTg1MjcxNjk3fQ.ZvgVjw2yH9Sd-s26XDjui1WX2XqV8uI3M71aXeIXZIk")
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Allow-Headers",
+                "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With,observe");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Expose-Headers", "Authorization");
+        response.addHeader("Access-Control-Expose-Headers", "responseType");
+        response.addHeader("Access-Control-Expose-Headers", "observe");
+        System.out.println("Request Method: " + request.getMethod());
+
+        String token = recuperarToken(request);
+        boolean valido = tokenService.isTokenValido(token);
+        if (valido) {
+            autenticarCliente(token);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private void autenticarCliente(String token) {
+        Long idUsuario = tokenService.getIdUsuario(token);
+        Usuario usuario = repository.findById(idUsuario).get();
+        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(usuario, null,
+                usuario.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+
+    private String recuperarToken(HttpServletRequest request) {
+        String token = request.getHeader("Authorization");
+        if (token == null || token.isEmpty() || !token.startsWith("Bearer ")) {
+            return null;
+        }
+
+        return token.substring(7, token.length());
+    }
 
 }
--- src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -46,9 +46,9 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers(HttpMethod.GET, "/topicos").permitAll()
                 .antMatchers(HttpMethod.GET, "/topicos/*").permitAll().antMatchers(HttpMethod.POST, "/auth").permitAll()
-                .antMatchers(HttpMethod.GET, "/actuator/**").permitAll().antMatchers(HttpMethod.GET, "/vets/**")
-                .permitAll().antMatchers(HttpMethod.GET, "/api/vets/**").permitAll().anyRequest().authenticated().and()
-                .csrf().disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .antMatchers(HttpMethod.GET, "/actuator/**").permitAll().antMatchers(HttpMethod.OPTIONS, "/**")
+                .permitAll().anyRequest().authenticated().and().csrf().disable().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .addFilterBefore(new AutenticacaoViaTokenFilter(tokenService, usuarioRepository),
                         UsernamePasswordAuthenticationFilter.class);
     }
